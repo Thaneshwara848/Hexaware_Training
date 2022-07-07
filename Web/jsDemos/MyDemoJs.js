f33885d99bda1f79d2d863026d5bfea213a9076c
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-07-07 17:00:11 +0530

    js

---
+++ Web/jsDemos/MyDemoJs.js
@@ -0,0 +1,11 @@
+    
+    function abc(){
+        console.log("abc function !")
+    }
+    function xyz(){
+        console.log("Xyz function ");
+    }
+    function atoz()
+    {
+            console.log("Ztoz function")
+    }
